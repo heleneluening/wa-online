1804c22bb55afaf8fb8ced874fa1505a17d67498
Lukasz <lukasz.szupka@student.fh-kiel.de>
2022-05-20 21:38:24 +0200

    Controls + Enemy and damage

--- controls.js
+++ controls.js
@@ -1,15 +1,15 @@
 const points = document.getElementsByClassName("point");
 let mouseIsDown = false;
-const symbol = [];
-
+const symbol = [];//symbol array
+//IMPORTANT MAIN FUNCTIONS: colorChange, castMagic, and death
 
 function colorChange(element) { //if mouseIsDown is true add new colour
     if (mouseIsDown) {
         element.classList.add("pointColour"); //changes colour
         symbol.push(element.id);
-        cast() // for magic to deal damage
+        castMagic() // for magic to deal damage
 
-        console.log(symbol);//TODO weg
+        console.log(symbol);//test if the symbol is drawn correct
     }
 }
 
@@ -20,7 +20,7 @@ function mouseDown() { //if mouse down mouseIsDown becomes true and triggers als
 function mouseUp() { // mouseIsDown becomes false and it removes the added colour
     mouseIsDown = false;
     for (const p of points) {
-        p.classList.remove("pointColour");
+        p.classList.remove("pointColour"); // removes the colour
     }
     symbol.length = 0;//delete array
 }
@@ -31,12 +31,37 @@ function pointClicked(element) { // add colour if you click on a point
 }
 
 
+let monsterHp = Math.floor(Math.random() * 10) + 1;//
+console.log(monsterHp); // check if it works
 
 
-function cast() {
-   if (symbol.length === 2 && symbol.includes("p1") && symbol.includes("p2")){ //symbol.includes("p1")&&symbol.includes("p2")&&!symbol.includes("p3")
-        console.log("hit");
-        // ["p1","p2"].every(it => symbol.includes(it))
-        symbol.length = 0; //prof nachfragen ob es ihn gefällt
+function castMagic() { //drawing symbols to cast magic // different soloution ["p1","p2"].every(it => symbol.includes(it))
+
+    if (symbol.length === 3 && symbol.includes("p1") && symbol.includes("p2") && symbol.includes("p3")) { //symbole 1,2,3
+        monsterHp = monsterHp - 1; //1 damage
+        console.log(monsterHp); //monsterHp check
+        symbol.length = 0; //delete the content of symbol array
+        death(); //if enemys has 0 hp he dies
+    }
+    if (symbol.length === 5 && symbol.includes("p2") && symbol.includes("p4") && symbol.includes("p5")
+        && symbol.includes("p6") && symbol.includes("p8")) { //2,4,5,6,8 Lightning
+
+        console.log("LightningBolt");
+        monsterHp = monsterHp - 5; //5 damage
+        console.log(monsterHp);
+        symbol.length = 0;
+        death();
     }
 }
+
+function death() { //function to check if monsterHp = 0 => death
+    if (monsterHp <= 0) {
+        console.log("Killed");
+        symbol.length = 0;
+    }
+}
+
+/*TODO more Enemys, create more Symobls and how much damage each deals
+    create a function of enemys so there are more enemys
+    let random = Math.floor(Math.random() * 10); random nr between 0 - 9
+    console.log(random);*/
