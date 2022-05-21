c99ebfd42e4f74e22188b09580987ad750e8d90c
Lukasz <lukasz.szupka@student.fh-kiel.de>
2022-05-21 15:45:06 +0200

    Controls + random Enemy + more Symobls

--- controls.js
+++ controls.js
@@ -15,7 +15,7 @@ function colorChange(element) { //if mouseIsDown is true add new colour
 
 function mouseDown() { //if mouse down mouseIsDown becomes true and triggers also colorChange
     mouseIsDown = true;
-}
+}//IDE says unused, ignore it!!!
 
 function mouseUp() { // mouseIsDown becomes false and it removes the added colour
     mouseIsDown = false;
@@ -32,28 +32,59 @@ function pointClicked(element) { // add colour if you click on a point
 
 //ab hier geht es um die monster/symbole/und den tod
 
-let monsterHp = Math.floor(Math.random() * 10) + 5;//monsterHp between 5-15
+let monsterHp = Math.floor(Math.random() * 5) +1;//monsterHp between 1-5
+let randomiser = monsterHp;
 console.log(monsterHp); // check if it works
 
+switch (randomiser) {
+    case 1:
+        monsterHp = 15;
+        break;
+    case 2:
+        monsterHp = 25
+        break;
+    case 3:
+        monsterHp = 30
+        break
+    case 4:
+        monsterHp = 35
+        break;
+    case 5:
+        monsterHp = 40
+        break;
+}
 
-function castMagic() { //drawing symbols to cast magic // different soloution ["p1","p2"].every(it => symbol.includes(it))
-// [0] after symbol means 1 position in the array
+console.log(monsterHp); // check if it works
+
+function castMagic() { //drawing symbols to cast magic // [0] after symbol means 1 position in the array
     if (symbol.length === 3 && symbol[0].includes("p1") && symbol[1].includes("p2")
-        && symbol[2].includes("p3")) { //symbole 1,2,3
+        && symbol[2].includes("p3")) {
         monsterHp = monsterHp - 1; //1 damage
         console.log(monsterHp); //monsterHp check
         symbol.length = 0; //delete the content of symbol array = reset
         death(); //if enemys has 0 hp he dies
-    }
+    }//symbole 1,2,3
     if (symbol.length === 5 && symbol[0].includes("p2") && symbol[1].includes("p4")
         && symbol[2].includes("p5") && symbol[3].includes("p6")
-        && symbol[4].includes("p8")) {//2,4,5,6,8 Lightning
+        && symbol[4].includes("p8")) {
         console.log("LightningBolt");
         monsterHp = monsterHp - 5; //5 damage
         console.log(monsterHp);
         symbol.length = 0;//reset of array
         death();
-    }
+    }//2,4,5,6,8 Lightning
+    if (symbol.length === 8  && symbol[0].includes("p1") && symbol[1].includes("p4")
+        && symbol[2].includes("p7") && symbol[3].includes("p8")
+        && symbol[4].includes("p9") && symbol[5].includes("p6")
+        && symbol[6].includes("p3") && symbol[7].includes("p2")){
+        console.log("Stone sword");
+        monsterHp = monsterHp - 7; //5 damage
+        console.log(monsterHp);
+        symbol.length = 0;//reset of array
+        death();
+    }//1,4,7,8,9,6,3,2 Stone
+
+
 }
 
 function death() { //functaion to check if monsterHp = 0 => death
@@ -64,6 +95,6 @@ function death() { //functaion to check if monsterHp = 0 => death
 }
 
 /*TODO more Enemys, create more Symobls and how much damage each deals
-    create a function of enemys so there are more enemys
     let random = Math.floor(Math.random() * 10); random nr between 0 - 9
-    console.log(random);*/
+    console.log(random);
+    */
