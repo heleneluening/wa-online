5569b6dea1a9cb75c5e4a366ceaad41843e6952d
Lukasz <lukasz.szupka@student.fh-kiel.de>
2022-05-24 14:49:47 +0200

    Controls + random Enemy + all in function

--- controls.js
+++ controls.js
@@ -1,6 +1,7 @@
 const points = document.getElementsByClassName("point");
 let mouseIsDown = false;
 const symbol = []; //symbol array
+let monsterTyp = Math.floor(Math.random() * 5) + 1; //random monsterTyp between 1-5
 //IMPORTANT MAIN FUNCTIONS: colorChange, castMagic, and death
 
 function colorChange(element) { //if mouseIsDown is true add new colour
@@ -31,46 +32,46 @@ function pointClicked(element) { //add colour if you click on a point
 } //add colour if you click on a point
 
 //from here are the symbols amd enemies
-//TODO add the following to a new function (onload maybe?)
-let monsterHp = Math.floor(Math.random() * 5) + 1; //random monsterHp between 1-5
-let randomiser = monsterHp; //random monsterHp
-console.log(monsterHp); // check if it works
+function enemies() {
+    let randomiser = monsterTyp; //random monsterTyp
+    console.log(monsterTyp); // check if it works
 
-switch (randomiser) {
-    case 1:
-        monsterHp = 15;
-        break;
-    case 2:
-        monsterHp = 25
-        break;
-    case 3:
-        monsterHp = 30
-        break
-    case 4:
-        monsterHp = 35
-        break;
-    case 5:
-        monsterHp = 40
-        break;
-} //randomised the enemy hp
+    switch (randomiser) {
+        case 1:
+            monsterTyp = 15;
+            break;
+        case 2:
+            monsterTyp = 25
+            break;
+        case 3:
+            monsterTyp = 30
+            break
+        case 4:
+            monsterTyp = 35
+            break;
+        case 5:
+            monsterTyp = 40
+            break;
+    } //randomised the enemy hp
 
-console.log(monsterHp); // check if it works
+    console.log(monsterTyp); // check if it works
+} //which enemie is spawned
 
 function castMagic() { //drawing symbols to cast magic, [0] after symbol means 1 position in the array
 
-    //symbole 6,5,4
+     //symbole 6,5,4
     if (symbol.length === 3 && symbol[0].includes("p6") && symbol[1].includes("p5")
         && symbol[2].includes("p4")) {
-        monsterHp = monsterHp - 1; //1 damage
-        console.log(monsterHp); //monsterHp check
+        monsterTyp = monsterTyp - 1; //1 damage
+        console.log(monsterTyp); //monsterTyp check
         symbol.length = 0; //delete the content of symbol array = reset
         death(); //if enemys has 0 hp he dies
     }
     //symbole 4,5,6
     if (symbol.length === 3 && symbol[0].includes("p4") && symbol[1].includes("p5")
         && symbol[2].includes("p6")) {
-        monsterHp = monsterHp - 1; //1 damage
-        console.log(monsterHp); //monsterHp check
+        monsterTyp = monsterTyp - 1; //1 damage
+        console.log(monsterTyp); //monsterTyp check
         symbol.length = 0; //delete the content of symbol array = reset
         death(); //if enemys has 0 hp he dies
     }
@@ -79,8 +80,8 @@ function castMagic() { //drawing symbols to cast magic, [0] after symbol means 1
         && symbol[2].includes("p5") && symbol[3].includes("p6")
         && symbol[4].includes("p8")) {
         console.log("LightningBolt");
-        monsterHp = monsterHp - 5; //5 damage
-        console.log(monsterHp);//test
+        monsterTyp = monsterTyp - 5; //5 damage
+        console.log(monsterTyp);//test
         symbol.length = 0;//reset of array
         death();
     }
@@ -90,8 +91,8 @@ function castMagic() { //drawing symbols to cast magic, [0] after symbol means 1
         && symbol[4].includes("p9") && symbol[5].includes("p6")
         && symbol[6].includes("p3") && symbol[7].includes("p2")) {
         console.log("Stone sword");
-        monsterHp = monsterHp - 7; //5 damage
-        console.log(monsterHp);//test
+        monsterTyp = monsterTyp - 7; //5 damage
+        console.log(monsterTyp);//test
         symbol.length = 0;//reset of array
         death();
     }
@@ -99,13 +100,17 @@ function castMagic() { //drawing symbols to cast magic, [0] after symbol means 1
 
 } //drawing symbols to cast magic, [0] after symbol means 1 position in the array
 
-function death() { //function to check if monsterHp = 0 => death
-    if (monsterHp <= 0) {
+function death() { //function to check if monsterTyp = 0 => death
+    if (monsterTyp <= 0) {
         console.log("Killed");//test
         symbol.length = 0;//reset of array
     }
-} //function to check if monsterHp = 0 => death
+} //function to check if monsterTyp = 0 => death
 
-/*TODO more Enemys, create more Symobls and how much damage each deals
+
+
+
+/*TODO create more Symboles and how much damage each deals
     console.log() = are test functions delete after finish
-    */
+    PlayerHp and damage to player
+*/
