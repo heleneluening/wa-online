15dc938a048407c2e183e9745d609c1ea8a6a4ba
Lukasz <lukasz.szupka@student.fh-kiel.de>
2022-06-03 15:58:28 +0200

    oop controls + animation with spam + undraggable

--- animation.js
+++ animation.js
@@ -4,30 +4,29 @@ let elem;
 
 
 function myMove() { //movement and style changing of #myAnimation
-    elem = document.createElement("div");
 
+    elem = document.createElement("div");
     elem.className = "myAnimation";
     let mainelem = document.getElementsByTagName("main");
     mainelem.item(0).appendChild(elem);
 
     switch (spellChoice) {
         case 1:
-            console.log("TEST 1") // elem = document.getElementById("myAnimation");?
+            console.log("TEST 1")
             elem.style.backgroundColor = "black"; // changes colour from spell
             elem.style.borderRadius = "10%";
             break;
         case 2:
-            console.log("TEST 2") // elem = document.getElementById("myAnimation");
+            console.log("TEST 2")
             elem.style.backgroundColor = "red";
             elem.style.borderRadius = "100%";
             break;
     }
-    elem.setAttribute("pos", "65"); // change pos it so it works everywhere
+    elem.setAttribute("pos", "65");
     elem.style.top = '65 vh';
     test.push(elem);
     clearInterval(id);
     id = setInterval(frame, 10);// 10 - 20
-
 
 
 }
@@ -37,15 +36,13 @@ function frame() {
         if (test[i].getAttribute("pos") <= 0) {
             test[i].remove();
             test.shift();
-            //elem.style.visibility = "hidden"; // hides the spell after it reaches it target
+
         } else {
             test[i].setAttribute("pos", test[i].getAttribute("pos")-1);
             test[i].style.top = test[i].getAttribute("pos") + 'vh';
 
             elem.style.visibility = "visible"; //makes the spell visible
         }
-        //elem.style.top = pos + 'vh';
-        //elem.style.left = pos - 'vw'; // not (needed)
     }
 
     if (test.length === 0) {
